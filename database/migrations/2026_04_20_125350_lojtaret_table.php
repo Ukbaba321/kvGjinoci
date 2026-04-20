c471402499300ce8987e52024c006b64b874e084
Diar programmeri <diaruka2020@gmail.com>
2026-04-20 15:12:24 +0200

    Main

---
+++ database/migrations/2026_04_20_125350_lojtaret_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lojtaret', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('emri');
+            $table->string('mbiemri');
+            $table->string('email');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->date('dataELindjes');
+            $table->integer('nrTelefonit');
+            $table->string('vendbanimi');
+            $table->integer('IBAN')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lojtaret');
+    }
+};
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <h1>TEST</h1>
+
+    <title> {{ config('app.name', 'kvGjinoci') }}</title>
+</head>
+<body class="text-red-500">
+    <p class="bg-red-500">TESTERBABA</p>
+
+        SHIT CODE
+
+    <span>BABAA</span>
+</body>
+</html>
